3cf84d95ec6accd4700016f3a233abf4fe9e91f6
Ruben Dorado <ruben.dorados@gmail.com>
2018-11-19 22:32:15 +0000

    Daily commit

--- stl.cpp
+++ stl.cpp
@@ -1,3 +1,23 @@
+#include<iostream>
+#include<vector>
+
+#include<libnlp/tokenizer.hpp>
+
+int main() {
+   std::vector<std::string> result;   
+   libnlp::Tokenizer tokenizer;    
+
+   tokenizer.tokenize(result, "Tokenize this string");      
+
+   for (std::vector<std::string>::iterator it = result.begin() ; it != result.end(); ++it)
+      std::cout << *it << std::endl;
+  
+   return 0;
+}
+
+
+
+/*
 #include<string>
 #include<vector>
 #include<iostream>
@@ -18,3 +38,5 @@ int main(){
   }
   return 0;
 }
+*/
+
